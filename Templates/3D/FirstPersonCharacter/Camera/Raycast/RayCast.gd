6f918021d00325cfd7aec71e294155dd880ed66e
Linko-3D <dbekhouc@gmail.com>
2019-06-10 23:48:44 +0200

    raycast

--- Templates/3D/FirstPersonCharacter/Camera/Raycast/RayCast.gd
+++ Templates/3D/FirstPersonCharacter/Camera/Raycast/RayCast.gd
@@ -4,4 +4,5 @@ extends RayCast
 func _input(event):
 	if event is InputEventMouseButton and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event.button_index == 1 and event.pressed == true:
-			print(get_collider())
+			if get_collider() != null:
+				get_collider().free()
