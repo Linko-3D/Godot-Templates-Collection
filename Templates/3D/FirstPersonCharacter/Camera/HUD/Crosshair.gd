8e9d3b2482ae820e8399384163d106ef1adf85c9
Linko <dbekhouc@gmail.com>
2019-06-12 15:07:48 +0200

    raycast improved

--- Templates/3D/FirstPersonCharacter/Camera/HUD/Crosshair.gd
+++ Templates/3D/FirstPersonCharacter/Camera/HUD/Crosshair.gd
@@ -1,14 +1,5 @@
 extends Sprite
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	position.x = OS.window_size.x / 2
 	position.y = OS.window_size.y / 2
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
