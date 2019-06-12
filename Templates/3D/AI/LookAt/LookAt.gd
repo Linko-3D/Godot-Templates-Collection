82a50348567460b68d634d5bf89a654d6ace60e2
Linko <dbekhouc@gmail.com>
2019-06-12 11:01:16 +0200

    tweak

--- Templates/3D/AI/LookAt/LookAt.gd
+++ Templates/3D/AI/LookAt/LookAt.gd
@@ -1,7 +1,8 @@
 extends Spatial
 
+var target = "FirstPersonCharacter"
 var playerPosition
 
 func _process(delta):
-	playerPosition = get_tree().get_root().find_node("FirstPersonCharacter", true, false)
+	playerPosition = get_tree().get_root().find_node(target, true, false)
 	look_at(playerPosition.translation, Vector3.UP)
