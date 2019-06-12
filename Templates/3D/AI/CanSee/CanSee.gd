aea0c86a969b06832fbd896dcb5bf9b81b9ddff5
Linko <dbekhouc@gmail.com>
2019-06-12 17:39:06 +0200

    tweak

--- Templates/3D/AI/CanSee/CanSee.gd
+++ Templates/3D/AI/CanSee/CanSee.gd
@@ -1,13 +1,7 @@
 extends Spatial
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var target = "FirstPersonCharacter"
+var targetPosition
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
