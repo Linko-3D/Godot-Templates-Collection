3592e01a15359ce82e0157bf2f8cf7b8f3c5492f
Linko <dbekhouc@gmail.com>
2019-06-12 11:36:13 +0200

    keep height

--- Templates/3D/AI/MoveTo/MoveTo.gd
+++ Templates/3D/AI/MoveTo/MoveTo.gd
@@ -4,8 +4,13 @@ var target = "FirstPersonCharacter"
 var targetPosition
 var vector = Vector3()
 var speed = 1
+var keepHeight = Vector3()
+
+func _ready():
+	keepHeight = translation.y
 
 func _process(delta):
 	targetPosition = get_tree().get_root().find_node(target, true, false)
 	vector = ((translation - targetPosition.translation)*-1).normalized()
+	translation.y = keepHeight
 	move_and_slide(vector * speed)
