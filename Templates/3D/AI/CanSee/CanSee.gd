a35e61af59dd20ce96039524ca62ee6810282a15
Linko <dbekhouc@gmail.com>
2019-06-13 17:02:07 +0200

    CanSee update

--- Templates/3D/AI/CanSee/CanSee.gd
+++ Templates/3D/AI/CanSee/CanSee.gd
@@ -4,4 +4,8 @@ var target
 
 func _process(delta):
 	target = get_tree().get_root().find_node("FirstPersonCharacter", true, false) # Get the target node by his name
-	look_at(target.translation, Vector3.UP) # Look at the target
+	#look_at(target.translation, Vector3.UP) # Look at the target
+	print($RayCast.get_collider())
+	
+	if $RayCast.get_collider() != null:
+		look_at(target.translation, Vector3.UP) # Look at the target
