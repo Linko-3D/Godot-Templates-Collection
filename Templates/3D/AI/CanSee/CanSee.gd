823148e5aa2b0c21078f07d6969bced8d3126a2e
Linko <dbekhouc@gmail.com>
2019-06-13 16:37:16 +0200

    fix

--- Templates/3D/AI/CanSee/CanSee.gd
+++ Templates/3D/AI/CanSee/CanSee.gd
@@ -1,7 +1,7 @@
 extends Spatial
 
-var target = "FirstPersonCharacter"
-var targetPosition
+var target
 
 func _process(delta):
-	pass
+	target = get_tree().get_root().find_node("FirstPersonCharacter", true, false) # Get the target node by his name
+	look_at(target.translation, Vector3.UP) # Look at the target
