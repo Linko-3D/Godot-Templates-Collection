a766b723aeabe3ec176baade9779153e14bb36e5
Linko <dbekhouc@gmail.com>
2019-06-13 13:42:34 +0200

    tweak

--- Templates/3D/AI/LookAt/LookAt.gd
+++ Templates/3D/AI/LookAt/LookAt.gd
@@ -1,8 +1,7 @@
 extends Spatial
 
-var target = "FirstPersonCharacter"
-var targetPosition
+var target
 
 func _process(delta):
-	targetPosition = get_tree().get_root().find_node(target, true, false) # Get the target node by his name
-	look_at(targetPosition.translation, Vector3.UP) # Look at the target
+	target = get_tree().get_root().find_node("FirstPersonCharacter", true, false) # Get the target node by his name
+	look_at(target.translation, Vector3.UP) # Look at the target
