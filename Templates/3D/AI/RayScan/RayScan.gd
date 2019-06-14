4ef535323a078088ffebac4a4e59314b2b9ddbdc
Linko <dbekhouc@gmail.com>
2019-06-14 18:30:33 +0200

    tweak

--- Templates/3D/AI/RayScan/RayScan.gd
+++ Templates/3D/AI/RayScan/RayScan.gd
@@ -3,11 +3,11 @@ extends RayCast
 var target
 var i = 0
 
-func _process(delta):
-	rotate_y(.1)
-	
+func _process(delta):	
 	target = get_tree().get_root().find_node("FirstPersonCharacter", true, false) # Get the target node by his name
 	
 	if get_collider() == target:
-		i+= 1
-		print(i, " I see you")
+		$AnimationPlayer.stop()
+		look_at(target.translation, Vector3.UP) # Look at the target
+	else:
+		$AnimationPlayer.play()
