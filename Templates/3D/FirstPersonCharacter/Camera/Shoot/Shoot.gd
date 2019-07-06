12a2dfd8d80aaee401d9e71a64744c2201f0f00b
Linko-3D <dbekhouc@gmail.com>
2019-07-06 15:21:56 +0200

    projectiles added

---
+++ Templates/3D/FirstPersonCharacter/Camera/Shoot/Shoot.gd
@@ -0,0 +1,12 @@
+extends Position3D
+
+var force = 20
+
+func _input(event): 
+	print(global_transform.basis.z)
+	if event is InputEventMouseButton and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event.button_index == 1 and event.pressed == true:
+			var bullet = load("res://Templates/3D/FirstPersonCharacter/Camera/Shoot/Projectile.tscn").instance()
+			add_child(bullet)
+			bullet.set_as_toplevel(true)
+			bullet.linear_velocity = global_transform.basis.z * -force
