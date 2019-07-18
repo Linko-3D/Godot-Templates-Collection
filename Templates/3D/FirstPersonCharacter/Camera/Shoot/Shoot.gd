4c80f600aa307c00f286985d82c5e77da738ec8f
Linko-3D <dbekhouc@gmail.com>
2019-07-18 14:26:40 +0200

    tweak

--- Templates/3D/FirstPersonCharacter/Camera/Shoot/Shoot.gd
+++ Templates/3D/FirstPersonCharacter/Camera/Shoot/Shoot.gd
@@ -5,7 +5,7 @@ var force = 25
 func _input(event): 
 	if event is InputEventMouseButton and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event.button_index == 1 and event.pressed == true:
-			var bullet = load("res://Templates/3D/FirstPersonCharacter/Camera/Shoot/Projectile.tscn").instance()
-			add_child(bullet)
-			bullet.set_as_toplevel(true)
-			bullet.linear_velocity = global_transform.basis.z * -force
+			var projectile = load("res://Templates/3D/FirstPersonCharacter/Camera/Shoot/Projectile.tscn").instance()
+			add_child(projectile)
+			projectile.set_as_toplevel(true)
+			projectile.linear_velocity = global_transform.basis.z * -force
