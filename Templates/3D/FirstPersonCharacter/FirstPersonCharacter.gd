ed26c9b1a21ac46ce7a2bbd538c62707e3fc48b0
Linko-3D <dbekhouc@gmail.com>
2019-07-18 17:37:52 +0200

    tweak

--- Templates/3D/FirstPersonCharacter/FirstPersonCharacter.gd
+++ Templates/3D/FirstPersonCharacter/FirstPersonCharacter.gd
@@ -21,13 +21,13 @@ func _physics_process(delta):
 
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if Input.is_action_pressed("ui_up"):
-			velocity += -global_transform.basis.z * speed
+			velocity += -transform.basis.z * speed
 		if Input.is_action_pressed("ui_down"):
-			velocity += global_transform.basis.z * speed
+			velocity += transform.basis.z * speed
 		if Input.is_action_pressed("ui_left"):
-			velocity += -global_transform.basis.x * speed
+			velocity += -transform.basis.x * speed
 		if Input.is_action_pressed("ui_right"):
-			velocity += global_transform.basis.x * speed
+			velocity += transform.basis.x * speed
 		
 		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 			velocity.y = jump_height
