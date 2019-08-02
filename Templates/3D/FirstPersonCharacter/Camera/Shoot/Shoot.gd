9f862a5ad7ee26196ded07241e42eb55d54899f9
Linko-3D <dbekhouc@gmail.com>
2019-08-02 19:53:20 +0200

    tweak

--- Templates/3D/FirstPersonCharacter/Camera/Shoot/Shoot.gd
+++ Templates/3D/FirstPersonCharacter/Camera/Shoot/Shoot.gd
@@ -3,14 +3,16 @@ extends Position3D
 var force = 25
 export (PackedScene) var bullet
 
+var projectile
+
+func _ready():
+	projectile = bullet.instance() # Avoid to have a lag on the first shoot by preloading the projectile
+
 func _input(event): 
 	if event is InputEventMouseButton and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event.button_index == 1 and event.pressed == true:
-			shoot()
-
-func shoot():
-	var projectile = bullet.instance() # We instance the scene
+			projectile = bullet.instance() # We instance the scene
 	
-	add_child(projectile) # The instance is added as a child of the shoot node
-	projectile.set_as_toplevel(true) # Projectile parented to the highest node in the level to detach it from the player
-	projectile.linear_velocity = global_transform.basis.z * -force # An initial force is applied when clicking, the force is applied on each new instance
+			add_child(projectile) # The instance is added as a child of the shoot node
+			projectile.set_as_toplevel(true) # Projectile parented to the highest node in the level to detach it from the player
+			projectile.linear_velocity = global_transform.basis.z * -force # An initial force is applied when clicking, the force is applied on each new instance
