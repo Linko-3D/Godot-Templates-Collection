abcb53604e6bf4bb5756dfee10cf8b25dcf6a6ef
Linko-3D <dbekhouc@gmail.com>
2019-08-06 22:01:54 +0200

    snap fixed

--- Templates/3D/FirstPersonCharacter/FirstPersonCharacter.gd
+++ Templates/3D/FirstPersonCharacter/FirstPersonCharacter.gd
@@ -7,7 +7,9 @@ var speed = 6
 var max_falling_speed = 20
 var jump_height = 4
 
-var snap = Vector3.DOWN
+var snap_distance = -0.1
+var snap = Vector3(0, snap_distance,0)
+
 
 var mouse_sensitivity = 0.15
 
@@ -35,7 +37,7 @@ func _physics_process(delta):
 			snap = Vector3()
 			velocity.y = jump_height
 		else:
-			snap = Vector3.DOWN
+			snap = Vector3(0, snap_distance, 0)
 		
 		velocity.y -= GRAVITY # Gravity
 		velocity.y = clamp(velocity.y, -max_falling_speed, max_falling_speed) # Max falling speed
@@ -43,7 +45,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
 		
-	velocity =  move_and_slide_with_snap(velocity, snap, Vector3.UP, true, 4, 90)
+	velocity =  move_and_slide_with_snap(velocity, snap, Vector3.UP, true, 4, 5)
 	
 # ----------------------------------
 # Mouse controls
