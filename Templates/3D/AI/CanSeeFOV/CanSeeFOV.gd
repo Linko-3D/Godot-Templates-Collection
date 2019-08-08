6317acc86876e67edb513b38ae1e40601b574628
Linko-3D <dbekhouc@gmail.com>
2019-08-08 18:08:23 +0200

    tweak

--- Templates/3D/AI/CanSeeFOV/CanSeeFOV.gd
+++ Templates/3D/AI/CanSeeFOV/CanSeeFOV.gd
@@ -1,16 +1,15 @@
 extends Spatial
 
 var target
-var angle_limit = 45
+var angle_limit_y = 45
 
 func _process(delta):
-	print(rotation_degrees.y)
 	target = get_tree().get_nodes_in_group("Player")[0] # Get the target with the first node in the Player group
 
-	if $RayCast.get_collider() == target and rotation_degrees.y <= angle_limit and rotation_degrees.y >= -angle_limit:
+	if $RayCast.get_collider() == target and rotation_degrees.y <= angle_limit_y and rotation_degrees.y >= -angle_limit_y:
 			look_at(target.translation, Vector3.UP) # Look at the target
 
-	if rotation_degrees.y > angle_limit:
-		rotation_degrees.y = angle_limit
-	if rotation_degrees.y < -angle_limit:
-		rotation_degrees.y = -angle_limit
+	if rotation_degrees.y > angle_limit_y:
+		rotation_degrees.y = angle_limit_y
+	if rotation_degrees.y < -angle_limit_y:
+		rotation_degrees.y = -angle_limit_y
