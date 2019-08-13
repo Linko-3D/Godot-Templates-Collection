54cb2a450b87566c4a95914631b0c1fe3f394e00
Linko <dbekhouc@gmail.com>
2019-08-13 11:15:33 +0200

    projectile sticky

--- Templates/3D/FirstPersonCharacter/Camera/Shoot/ProjectileSticky.gd
+++ Templates/3D/FirstPersonCharacter/Camera/Shoot/ProjectileSticky.gd
@@ -1,16 +1,27 @@
 extends RigidBody
 
 var lifespan = 3
+var target
+var target_coordinates = Vector3()
+var current_coordinates = Vector3()
+var offset = Vector3()
 
 func _ready():
-	friction = 0
 	$Timer.wait_time = lifespan
 	$Timer.start()
+
+func _process(delta):
+	if target != null:
+		translation = target.translation + offset
 
 func _on_Timer_timeout():
 	queue_free()
 
 func _on_Area_body_entered(body):
+	target = body
+	target_coordinates = body.translation
+	offset = translation - target_coordinates
+	axis_lock_linear_z = true
 	axis_lock_linear_x = true
 	axis_lock_linear_y = true
-	axis_lock_linear_z = true
+	$Area.monitoring = false
