48968c789085657dfb98e22d4be9ad81f844552c
Linko-3D <dbekhouc@gmail.com>
2019-08-18 14:54:11 +0200

    tweak

--- Templates/3D/AI/MoveAB/MoveAB.gd
+++ Templates/3D/AI/MoveAB/MoveAB.gd
@@ -3,5 +3,5 @@ extends StaticBody
 export (Vector3) var MoveTo
 
 func _process(delta):
-	#$Tween.interpolate_property(self, translation, translation, MoveTo, 1.0, 0, 0 ,0)
-	pass
+	print(translation)
+	$Tween.interpolate_property(self, "translation", translation, MoveTo, 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
