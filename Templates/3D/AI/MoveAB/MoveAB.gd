5142e0e7a8c85fcc10412a0c745584cee062cf1b
Linko-3D <dbekhouc@gmail.com>
2019-08-18 18:03:08 +0200

    tweak

--- Templates/3D/AI/MoveAB/MoveAB.gd
+++ Templates/3D/AI/MoveAB/MoveAB.gd
@@ -1,7 +1,19 @@
-extends StaticBody
+extends KinematicBody
 
 export (Vector3) var MoveTo
+var forward = true
+var starting_position
+
+func _ready():
+	starting_position = translation
 
 func _process(delta):
-	print(translation)
-	$Tween.interpolate_property(self, "translation", translation, MoveTo, 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
+	if $Tween.is_active() == false: # Each time the animation stops check the direction set
+		if forward:
+			$Tween.start()
+			$Tween.interpolate_property(self, "translation", translation, MoveTo, 2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0)
+			forward = false
+		else:
+			$Tween.start()
+			$Tween.interpolate_property(self, "translation", translation, starting_position, 2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0)
+			forward = true
