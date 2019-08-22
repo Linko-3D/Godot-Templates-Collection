8f4df53137db90fd516f928ae8ccb04aa923c7a3
Linko-3D <dbekhouc@gmail.com>
2019-08-22 17:09:15 +0200

    tweak

--- Templates/Singleton.gd
+++ Templates/Singleton.gd
@@ -5,7 +5,7 @@ var spawn_location = Vector3()
 
 func _ready():
 	player = get_tree().get_nodes_in_group("Player")[0] # Get the target with the first node in the Player group
-	spawn_location = player.translation # Savng the starting position from the editor
+	spawn_location = player.translation # Saving the starting position from the editor
 
 func _process(delta):
 	print(spawn_location)
@@ -13,7 +13,7 @@ func _process(delta):
 	if Input.is_action_just_pressed("restart"):
 		get_tree().reload_current_scene()
 		player = get_tree().get_nodes_in_group("Player")[0] # Get the target with the first node in the Player group
-		player.translation = spawn_location # Latest spawn location registered
+		player.translation = spawn_location # Using the latest spawn location saved
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
