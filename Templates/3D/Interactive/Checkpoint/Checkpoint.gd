52fc1971d5444d91b1f86cc5c6840924d09492a5
Linko-3D <dbekhouc@gmail.com>
2019-08-22 17:39:36 +0200

    tweak

--- Templates/3D/Interactive/Checkpoint/Checkpoint.gd
+++ Templates/3D/Interactive/Checkpoint/Checkpoint.gd
@@ -2,4 +2,4 @@ extends Area
 
 func _on_Checkpoint_body_entered(body):
 	if body.is_in_group("Player"):
-		Singleton.spawn_location = translation # Uses the current location of the checkpoint and use it as the new player spawn
+		GLOBAL.spawn_location = translation # Uses the current location of the checkpoint and use it as the new player spawn
