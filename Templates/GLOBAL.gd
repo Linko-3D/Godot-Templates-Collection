cf956dcebc324387ac8ab6f8edc5f1c9b1e901ed
Linko-3D <dbekhouc@gmail.com>
2019-08-22 19:54:01 +0200

    tweak

--- Templates/GLOBAL.gd
+++ Templates/GLOBAL.gd
@@ -6,13 +6,14 @@ var spawn_location = Vector3()
 func _ready():
 	player = get_tree().get_nodes_in_group("Player")[0] # Get the target with the first node in the Player group
 	spawn_location = player.translation # Saving the starting position from the editor
+	print(player)
 
 func _process(delta):
-	print(spawn_location)
 	
 	if Input.is_action_just_pressed("restart"):
 		get_tree().reload_current_scene()
 		player = get_tree().get_nodes_in_group("Player")[0] # Get the target with the first node in the Player group
+		print(player)
 		player.translation = spawn_location # Using the latest spawn location saved
 	
 	if Input.is_action_just_pressed("ui_cancel"):
