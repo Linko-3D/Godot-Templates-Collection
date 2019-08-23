20c61113be888514e7beb8e9793a75ea258e086d
Linko <dbekhouc@gmail.com>
2019-08-23 10:35:44 +0200

    tweak

--- Templates/References.gd
+++ Templates/References.gd
@@ -2,3 +2,5 @@ get_tree().get_nodes_in_group("Player")[0] # Get the first node in the group "Pl
 target = get_tree().get_root().find_node("Player", true, false) # Find the node with the name "Player"
 
 if "variable" in body: # check if there is a variable
+
+yield(get_tree().create_timer(3), "timeout") # Wait 3 seconds
