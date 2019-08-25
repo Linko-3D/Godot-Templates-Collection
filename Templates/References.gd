b7d40e60f9b05206243ac2f5d31dfbfc87d998f5
Linko-3D <dbekhouc@gmail.com>
2019-08-25 23:13:41 +0200

    tweak

--- Templates/References.gd
+++ Templates/References.gd
@@ -4,3 +4,6 @@ target = get_tree().get_root().find_node("Player", true, false) # Find the node 
 if "variable" in body: # check if there is a variable
 
 yield(get_tree().create_timer(3), "timeout") # Wait 3 seconds
+
+randomize()
+rand_range(float from, float to)
