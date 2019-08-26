1450acfbb643fe7cef335f13361641ff150cb8c2
Linko-3D <dbekhouc@gmail.com>
2019-08-26 16:44:57 +0200

    tweak

--- Templates/3D/Others/Hook/Hook.gd
+++ Templates/3D/Others/Hook/Hook.gd
@@ -1,17 +1,25 @@
 extends RayCast
 
-var target
+export var pull_force = 20.0
+
+var character
+var destination
 var vector
+var hold = false
 
 func _ready():
-	target = get_tree().get_nodes_in_group("Player")[0] # Get the first node in the group "Player"
+	character = get_tree().get_nodes_in_group("Player")[0] # Get the first node in the group "Player"
+
+func _process(delta):
+	if hold:
+		vector = (destination - character.translation).normalized()
+		character.move_and_slide(vector * pull_force)
 
 func _input(event):
 	if event is InputEventMouseButton and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event.button_index == 2 and event.pressed == true:
 			if is_colliding():
-				vector = (get_collision_point() - target.translation).normalized()
-				$Tween.start()
-				$Tween.interpolate_property(target, "translation", target.translation, get_collision_point(), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0)
+				destination = get_collision_point()
+				hold = true
 		if event.button_index == 2 and event.pressed == false:
-			$Tween.stop_all()
+			hold = false
