c394d57f4f24752444f56d822ed5dd9cb80553e8
Linko <dbekhouc@gmail.com>
2019-08-27 09:54:53 +0200

    tweak

--- Templates/3D/Interactive/Pickable/Pickable.gd
+++ Templates/3D/Interactive/Pickable/Pickable.gd
@@ -2,6 +2,10 @@ extends RigidBody
 
 var can_pick = false
 
+func _process(delta):
+	if can_pick and Input.is_action_just_pressed("Interact"):
+		print("Picking")
+
 func _on_Area_body_entered(body):
 	if body.is_in_group("Player"):
 		can_pick = true
