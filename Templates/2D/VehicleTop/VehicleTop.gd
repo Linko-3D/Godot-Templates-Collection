8c01cfe0dc4923439b7f949e9337fbc80158d357
Linko-3D <dbekhouc@gmail.com>
2019-08-29 17:37:16 +0200

    tweak

--- Templates/2D/VehicleTop/VehicleTop.gd
+++ Templates/2D/VehicleTop/VehicleTop.gd
@@ -5,19 +5,19 @@ var speed = 300
 var rot_speed = 0.05
 
 func _process(delta):
-	print(deg2rad(90))
 	velocity = Vector2()
 	
 	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2(speed, 0).rotated(rotation-deg2rad(90))
+		velocity.y = -speed
 	if Input.is_action_pressed("ui_down"):
-		velocity = Vector2(-speed, 0).rotated(rotation-deg2rad(90))
+		velocity.y = speed
 	
 	if Input.is_action_pressed("ui_right"):
 		rotate(rot_speed)
 	if Input.is_action_pressed("ui_left"):
 		rotate(-rot_speed)
 	
+	velocity = velocity.rotated(rotation)
 	move_and_slide(velocity)
 
 func _input(event):
