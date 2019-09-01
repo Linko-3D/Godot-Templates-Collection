aef7f69ac8202c608d12a1ca3944adf4f964ab7e
Linko-3D <dbekhouc@gmail.com>
2019-09-01 19:09:38 +0200

    tweak

--- Templates/3D/Interactive/MoveRandom/MoveRandom.gd
+++ Templates/3D/Interactive/MoveRandom/MoveRandom.gd
@@ -1,5 +1,24 @@
 extends StaticBody
 
+var max_distance = 2
+var moveTo = Vector3()
+var speed = 0.1
+var pause = 1
+
+var limitBottom = 1
+
+func _ready():
+	moveTo = translation
+
 func _process(delta):
+	if translation.y < limitBottom:
+		translation.y = limitBottom
+
 	if $Tween.is_active() == false: # Each time the animation stops check the direction set
 		$Tween.start()
+		moveTo = translation
+		randomize()
+		moveTo.x += rand_range(-max_distance, max_distance)
+		moveTo.y += rand_range(-max_distance, max_distance)
+		moveTo.z += rand_range(-max_distance, max_distance)
+		$Tween.interpolate_property(self, "translation", translation, moveTo, speed, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, pause)
