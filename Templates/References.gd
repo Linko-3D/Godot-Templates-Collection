65bc498f2a4df34d3ef3d4f3d0dbb9e34629500c
Linko-3D <dbekhouc@gmail.com>
2019-09-15 12:13:40 +0200

    tweak

--- Templates/References.gd
+++ Templates/References.gd
@@ -1,10 +1,12 @@
-Use in a ready process:
-get_tree().get_nodes_in_group("Player")[0] # Get the first node in the group "Player"
-
+#Use both in a ready process:
+get_tree().get_nodes_in_group("Player")[0] # Get the first node in the group "Player" (recommended)
 target = get_tree().get_root().find_node("Player", true, false) # Find the node with the name "Player"
 
-if "variable" in body: # check if there is a variable
+# Ideal with area nodes:
+if "variable" in body: # check if the node has this variable
+if method() in body: # check if the node has this method
 
+# Ideal for projectiles
 yield(get_tree().create_timer(3), "timeout") # Wait 3 seconds
 
 randomize()
