a2b10ebddc8f9dbcf23f7ece10d609b06ad761e9
Linko-3D <dbekhouc@gmail.com>
2019-09-15 20:22:10 +0200

    tweak

--- Templates/3D/AI/Sentry/Sentry.gd
+++ Templates/3D/AI/Sentry/Sentry.gd
@@ -4,11 +4,13 @@ var target
 var angle
 var speed = 0.1
 
+func _ready():
+	target = get_tree().get_nodes_in_group("Player")[0] # Get the target with the first node in the Player group
+
 func _process(delta):
 	rotation.x = 0
 	rotation.z = 0
 	rotate_y(speed)
-	target = get_tree().get_nodes_in_group("Player")[0] # Get the target with the first node in the Player group
 
 	if $RayCast.get_collider() == target:
 		look_at(target.translation, Vector3.UP) # Look at the target
