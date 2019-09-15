9e28f91787a01a6c20d13d81579994778a768a7e
Linko-3D <dbekhouc@gmail.com>
2019-09-15 22:00:29 +0200

    tweak

--- Templates/3D/Shoot/Projectile/ProjectileImpact.gd
+++
@@ -1,7 +0,0 @@
-extends RigidBody
-
-var lifespan = 3
-
-func _ready():
-	yield(get_tree().create_timer(lifespan), "timeout")
-	queue_free()
--- Templates/3D/Shoot/Projectile/ProjectileSticky.gd
+++ Templates/3D/Shoot/Projectile/ProjectileSticky.gd
@@ -10,11 +10,20 @@ func _ready():
 	yield(get_tree().create_timer(lifespan), "timeout")
 	queue_free()
 
-func _on_Area_body_entered(body):
+
+
+#func _on_Area_body_entered(body):
+#	if body.is_in_group("Player") == false:
+#		target = body
+#		target_coordinates = body.translation
+#		offset = translation - target_coordinates
+#		axis_lock_linear_z = true
+#		axis_lock_linear_x = true
+#		axis_lock_linear_y = true
+
+func _on_ProjectileSticky_body_entered(body):
 	if body.is_in_group("Player") == false:
-		target = body
-		target_coordinates = body.translation
-		offset = translation - target_coordinates
 		axis_lock_linear_z = true
 		axis_lock_linear_x = true
 		axis_lock_linear_y = true
+		
