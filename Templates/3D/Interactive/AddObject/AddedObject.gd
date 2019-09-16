09f3eaf751466b869d18daba705487761dd51bcd
Linko-3D <dbekhouc@gmail.com>
2019-09-16 17:43:21 +0200

    tweak

--- Templates/3D/Interactive/AddObject/AddedObject.gd
+++ Templates/3D/Interactive/AddObject/AddedObject.gd
@@ -2,3 +2,7 @@ extends StaticBody
 
 func _process(delta):
 	rotation = Vector3()
+	
+	translation.x = round(translation.x)
+	translation.y = round(translation.y)
+	translation.z = round(translation.z)
