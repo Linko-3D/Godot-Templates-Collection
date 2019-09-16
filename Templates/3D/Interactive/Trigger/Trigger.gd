af7f3f697b00b486c038cd0fcb57e6bc8affd47c
Linko-3D <dbekhouc@gmail.com>
2019-09-16 14:09:18 +0200

    tweak

--- Templates/3D/Interactive/Trigger/Trigger.gd
+++ Templates/3D/Interactive/Trigger/Trigger.gd
@@ -2,11 +2,14 @@ extends Area
 
 export var ID = 0
 
+var enable = false
+
 func _on_Interuptor_body_entered(body):
 	if body.is_in_group("Player"):
-		get_tree().call_group("Triggered", "Trigger(ID)")
-		print("inside")
+		enable = true
+		get_tree().call_group("Triggered", "trigger", ID, enable)
 
 func _on_Interuptor_body_exited(body):
 	if body.is_in_group("Player"):
-		print("outside")
+		enable = false
+		get_tree().call_group("Triggered", "trigger", ID, enable)
--- Templates/3D/Interactive/Trigger/Triggered.gd
+++ Templates/3D/Interactive/Trigger/Triggered.gd
@@ -2,5 +2,11 @@ extends OmniLight
 
 export var ID = 0
 
-func triggered(ID):
-	light_energy = 16
+func trigger(triggerID, enable):
+	if triggerID == ID:
+		if enable:
+			light_energy = 16
+			light_indirect_energy = 16
+		else:
+			light_energy = 0
+			light_indirect_energy = 0
