446bad6bbfe82240a989c8c9bd1d3314a91d0dd2
Linko-3D <dbekhouc@gmail.com>
2019-09-20 22:09:53 +0200

    tweak

--- Templates/3D/Interactive/Carry/Carry.gd
+++ Templates/3D/Interactive/Carry/Carry.gd
@@ -1,10 +1,13 @@
 extends RigidBody
 
 var can_pick = false
+var holding = false
 
 func _process(delta):
 	if can_pick and Input.is_action_just_pressed("interact"):
-		print("Picking")
+		holding = true
+	if holding:
+		global_transform = get_tree().get_root().find_node("Hold", true, false).get_global_transform()
 
 func _on_Area_body_entered(body):
 	if body.is_in_group("Player"):
