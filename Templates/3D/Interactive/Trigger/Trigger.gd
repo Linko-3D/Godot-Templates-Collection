a90c339b78e8109db8ed721e4337ca2e0629563a
Linko-3D <dbekhouc@gmail.com>
2019-09-20 17:52:22 +0200

    tweak

--- Templates/3D/Interactive/Trigger/Trigger.gd
+++ Templates/3D/Interactive/Trigger/Trigger.gd
@@ -1,15 +1,16 @@
 extends Area
 
+export var active = true
 export var ID = 0
 
 var enable = false
 
 func _on_Interuptor_body_entered(body):
-	if body.is_in_group("Player"):
+	if active and body.is_in_group("Player"):
 		enable = true
 		get_tree().call_group("Triggered", "trigger", ID, enable)
 
 func _on_Interuptor_body_exited(body):
-	if body.is_in_group("Player"):
+	if active and body.is_in_group("Player"):
 		enable = false
 		get_tree().call_group("Triggered", "trigger", ID, enable)
