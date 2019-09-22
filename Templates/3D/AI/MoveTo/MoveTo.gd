b479647b40e7198686e80e27d6b0476b151233c7
Linko-3D <dbekhouc@gmail.com>
2019-09-22 21:11:12 +0200

    tweak

--- Templates/3D/AI/MoveTo/MoveTo.gd
+++ Templates/3D/AI/MoveTo/MoveTo.gd
@@ -1,5 +1,9 @@
 extends KinematicBody
 
+export var speed = 1
+export var lock_linear_y = true
+export var lock_angular_x = true
+
 var target
 var vector = Vector3()
 var keepHeight = Vector3()
@@ -11,6 +15,12 @@ func _ready():
 func _process(delta):
 	vector = (target.translation - translation).normalized() # Get the direction by subtracting the current position by the target position
 	
-	translation.y = keepHeight # Keep the same height
+	if lock_linear_y:
+		translation.y = keepHeight # Keep the same height
 	
-	move_and_slide(vector)
+	move_and_slide(vector * speed)
+	
+	look_at(target.translation, Vector3.UP) # Look at the target
+
+	if lock_angular_x:
+		rotation.x = 0
