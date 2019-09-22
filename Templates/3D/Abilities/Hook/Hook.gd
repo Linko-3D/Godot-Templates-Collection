8353ee4a2eb5ef6f41c520215155f18e1fd15020
Linko-3D <dbekhouc@gmail.com>
2019-09-22 22:10:18 +0200

    tweak

--- Templates/3D/Abilities/Hook/Hook.gd
+++ Templates/3D/Abilities/Hook/Hook.gd
@@ -18,7 +18,7 @@ func _process(delta):
 func _input(event):
 	if event is InputEventMouseButton and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event.button_index == 2 and event.pressed == true:
-			if is_colliding() and get_collider().is_in_group("Boundaries") == false:
+			if is_colliding() and get_collider().get_class() != "RigidBody":
 				destination = get_collision_point()
 				hold = true
 		if event.button_index == 2 and event.pressed == false:
