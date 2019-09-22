4fcf99fd91c2988655550319ad2acd78ff68ef43
Linko-3D <dbekhouc@gmail.com>
2019-09-22 20:42:21 +0200

    tweak

--- Templates/3D/Interactive/Grab/Grab.gd
+++ Templates/3D/Interactive/Grab/Grab.gd
@@ -1,17 +1,21 @@
 extends RayCast
 
+export var mass_limit = 30
+
 var object_grabbed = null
 
 func _process(delta):
-	if Input.is_action_just_pressed("interact"):
+	if Input.is_action_just_pressed("interact") and object_grabbed != null:
+		get_collider().set_mode(0)
 		object_grabbed = null
 		enabled = true
 
 	if is_colliding():
-		if get_collider().get_class() == "RigidBody":
+		if get_collider().get_class() == "RigidBody" and get_collider().mass <= mass_limit:
 			if Input.is_action_just_pressed("interact"):
 				object_grabbed = get_collider()
 				enabled = false
 
 	if object_grabbed != null:
+		get_collider().set_mode(1)
 		get_collider().global_transform = $Hold.global_transform
