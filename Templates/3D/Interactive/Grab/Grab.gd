504e410ca35137dfeae63baab26aa0bae97cd5d1
Linko-3D <dbekhouc@gmail.com>
2019-09-24 00:53:00 +0200

    tweak

--- Templates/3D/Interactive/Grab/Grab.gd
+++ Templates/3D/Interactive/Grab/Grab.gd
@@ -6,7 +6,7 @@ var object_grabbed = null
 
 func _process(delta):
 	if Input.is_action_just_pressed("interact") and object_grabbed != null:
-		get_collider().set_mode(0)		# We set the object to RigidBody again to resets the gravity
+		get_collider().set_mode(0)		# We set the object to RigidBody again to reset the gravity
 		get_collider().can_sleep = false
 		object_grabbed = null
 		enabled = true
@@ -24,6 +24,6 @@ func _input(event):
 	if event is InputEventMouseButton and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event.button_index == 1 and event.pressed == true:
 			if object_grabbed != null:
-				get_collider().set_mode(0)	# We set the object to RigidBody again to resets the gravity
+				get_collider().set_mode(0)	# We set the object to RigidBody again to reset the gravity
 				object_grabbed = null
 				enabled = true
