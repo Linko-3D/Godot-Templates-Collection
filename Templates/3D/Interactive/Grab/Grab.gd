d49252fd74734c4db9cdb573200e10c88aa4ea9b
Linko-3D <dbekhouc@gmail.com>
2019-09-29 15:25:09 +0200

    tweak

--- Templates/3D/Interactive/Grab/Grab.gd
+++ Templates/3D/Interactive/Grab/Grab.gd
@@ -5,17 +5,19 @@ export var mass_limit = 50
 var object_grabbed = null
 
 func _process(delta):
-	if Input.is_action_just_pressed("interact") and object_grabbed != null:
-		get_collider().set_mode(0)		# We set the object to RigidBody again to reset the gravity
-		get_collider().can_sleep = false
-		object_grabbed = null
-		enabled = true
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if Input.is_action_just_pressed("interact") and object_grabbed != null:
+			get_collider().set_mode(0)		# We set the object to RigidBody again to reset the gravity
+			get_collider().can_sleep = false
+			object_grabbed = null
+			enabled = true
 
 	if is_colliding():
 		if get_collider().get_class() == "RigidBody" and get_collider().mass <= mass_limit:
-			if Input.is_action_just_pressed("interact"):
-				object_grabbed = get_collider()
-				enabled = false
+			if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+				if Input.is_action_just_pressed("interact"):
+					object_grabbed = get_collider()
+					enabled = false
 
 	if object_grabbed != null:
 		get_collider().global_transform = $Hold.global_transform
--- Templates/3D/Interactive/GroundButton/GroundButton.gd
+++ Templates/3D/Interactive/GroundButton/GroundButton.gd
@@ -1,15 +1,23 @@
 extends Area
 
-export var mass_trigger = 50
+export var mass_trigger = 100
+var total_mass = 0
 
 func _on_GroundButton_body_entered(body):
 	if body.get_class() == "RigidBody":
-		print(body.mass, " kg")
-		if body.mass >= mass_trigger:
-			print("Triggered")
+		print("+", body.mass, "kg")
+		total_mass += body.mass
+		print("Total mass: ", total_mass, "kg\n")
+		
+		if total_mass >= mass_trigger:
+			print("** Triggered **\n")
 
 
 func _on_GroundButton_body_exited(body):
 	if body.get_class() == "RigidBody":
-		if body.mass >= mass_trigger:
-			print("Untriggered")
+		print("-", body.mass, "kg")
+		total_mass -= body.mass
+		print("Total mass: ", total_mass, "kg\n")
+	
+	if total_mass < mass_trigger:
+		print("** Untriggered** \n")
