2a8dfa6a0d0b14cc23c8af5a8b07e28ef8a195a2
Linko-3D <dbekhouc@gmail.com>
2019-09-29 20:10:41 +0200

    tweak

--- Templates/3D/Interactive/GroundButton/GroundButton.gd
+++ Templates/3D/Interactive/GroundButton/GroundButton.gd
@@ -1,29 +1,22 @@
 extends Area
 
-export var trigger_mass = 100
+export var trigger_mass = 40
 export var trigger_ID = 0
 
 var total_mass = 0
-var enable = false
 
 func _on_GroundButton_body_entered(body):
 	if body.get_class() == "RigidBody":
-		print("+", body.mass, "kg")
 		total_mass += body.mass
-		print("Total mass: ", total_mass, "kg\n")
 
 		if total_mass >= trigger_mass:
-			enable = true
-			get_tree().call_group("Triggered", "trigger", trigger_ID, enable)
+			get_tree().call_group("Triggered", "trigger", trigger_ID, true)
 			$Base/Button.translation.y = -0.1
 
 func _on_GroundButton_body_exited(body):
 	if body.get_class() == "RigidBody":
-		print("-", body.mass, "kg")
 		total_mass -= body.mass
-		print("Total mass: ", total_mass, "kg\n")
 
 	if total_mass < trigger_mass:
-		enable = false
-		get_tree().call_group("Triggered", "trigger", trigger_ID, enable)
+		get_tree().call_group("Triggered", "trigger", trigger_ID, false)
 		$Base/Button.translation.y = 0
--- Templates/3D/Interactive/Trigger/Trigger.gd
+++ Templates/3D/Interactive/Trigger/Trigger.gd
@@ -3,14 +3,10 @@ extends Area
 export var active = true
 export var trigger_ID = 0
 
-var enable = false
-
 func _on_Interuptor_body_entered(body):
 	if active and body.is_in_group("Player"):
-		enable = true
-		get_tree().call_group("Triggered", "trigger", trigger_ID, enable)
+		get_tree().call_group("Triggered", "trigger", trigger_ID, true)
 
 func _on_Interuptor_body_exited(body):
 	if active and body.is_in_group("Player"):
-		enable = false
-		get_tree().call_group("Triggered", "trigger", trigger_ID, enable)
+		get_tree().call_group("Triggered", "trigger", trigger_ID, false)
