d4516469dd4a8ab6515c70f68b5295502dc669bc
Linko-3D <dbekhouc@gmail.com>
2019-09-29 12:44:34 +0200

    tweak

---
+++ Templates/todo.gd
@@ -0,0 +1,15 @@
+checkpoint
+bullet trail from raycast
+nozzle flash
+display damage numbers
+zipline
+jump pad
+wall jump
+pocket teleporters
+swing object
+auto aim with angle limit
+aim at colision point with angle limit
+run on walls
+grappling hook swing
+throw object
+pull like HL2
