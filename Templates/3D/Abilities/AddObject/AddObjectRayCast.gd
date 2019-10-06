faa7526f81f5eb5fd165167bddbc33f209bfd1f1
Linko-3D <dbekhouc@gmail.com>
2019-10-06 21:24:28 +0200

    tweak

--- Templates/3D/Abilities/AddObject/AddObjectRayCast.gd
+++ Templates/3D/Abilities/AddObject/AddObjectRayCast.gd
@@ -1,12 +1,12 @@
 extends RayCast
 
-export (PackedScene) var object
+export (PackedScene) var object1
 
 func _input(event):
 	if event is InputEventMouseButton and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event.button_index == 3 and event.pressed == true:
 			if is_colliding():
-				var object_instance = object.instance() # We instance the scene
+				var object_instance = object1.instance() # We instance the scene
 		
 				add_child(object_instance) # The instance is added as a child of the shoot node
 				object_instance.set_as_toplevel(true)
