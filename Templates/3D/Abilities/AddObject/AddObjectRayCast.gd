2dde6532f5bbdb9a99df54e88efed54627664150
Linko-3D <dbekhouc@gmail.com>
2019-10-07 21:05:15 +0200

    tweak

--- Templates/3D/Abilities/AddObject/AddObjectRayCast.gd
+++ Templates/3D/Abilities/AddObject/AddObjectRayCast.gd
@@ -4,6 +4,16 @@ export (PackedScene) var object1
 
 var snap = Vector3()
 
+var player
+var object_rotation = Vector3()
+
+func _ready():
+	player = get_tree().get_nodes_in_group("Player")[0] # Get the target with the first node in the Player group
+
+func _process(delta):
+	
+	object_rotation.y = stepify(player.rotation_degrees.y, 90)
+	#print(object_rotation)
 func _input(event):
 	if event is InputEventMouseButton and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event.button_index == 3 and event.pressed == true:
@@ -18,3 +28,4 @@ func _input(event):
 				add_child(object_instance) # The instance is added as a child of the shoot node
 				object_instance.set_as_toplevel(true)
 				object_instance.translation = snap
+				object_instance.rotation_degrees = object_rotation
--- Templates/3D/Abilities/AddObject/Object1.gd
+++
@@ -1,6 +0,0 @@
-extends StaticBody
-
-func _process(delta):
-	rotation_degrees.y = stepify(rotation_degrees.y, 90)
-	rotation.x = 0
-	rotation.z = 0
