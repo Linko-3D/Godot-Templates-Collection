4e789558b4f75320b14debb075f44e791b57e800
Linko-3D <dbekhouc@gmail.com>
2019-10-07 20:20:23 +0200

    tweak

--- Templates/GLOBAL.gd
+++ Templates/GLOBAL.gd
@@ -9,11 +9,12 @@ func _ready():
 	checkpoint = player.translation # Saving the starting position of the player for the initial checkpoint
 
 func _process(delta):
+	player = get_tree().get_nodes_in_group("Player")[0]		# Get the target with the first node in the Player group
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if Input.is_action_just_pressed("restart"):
 			get_tree().reload_current_scene()
-			player = get_tree().get_nodes_in_group("Player")[0]		# Get the target with the first node in the Player group
-			player.global_transform.origin = checkpoint						# Using the latest spawn location saved
+			yield(get_tree().create_timer(0.1), "timeout")
+			player.translation = checkpoint						# Using the latest spawn location saved
 		if Input.is_action_just_pressed("ui_cancel"):
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	else:
