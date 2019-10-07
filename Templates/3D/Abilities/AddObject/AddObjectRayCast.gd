a70edbbb6e86253d1cdbfcccdb10d00f8c45535b
Linko-3D <dbekhouc@gmail.com>
2019-10-07 20:24:34 +0200

    tweak

--- Templates/3D/Abilities/AddObject/AddObjectRayCast.gd
+++ Templates/3D/Abilities/AddObject/AddObjectRayCast.gd
@@ -2,12 +2,19 @@ extends RayCast
 
 export (PackedScene) var object1
 
+var snap = Vector3()
+
 func _input(event):
 	if event is InputEventMouseButton and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event.button_index == 3 and event.pressed == true:
 			if is_colliding():
 				var object_instance = object1.instance() # We instance the scene
-		
+				
+				snap = get_collision_point()
+				snap.x = stepify(get_collision_point().x, 4)
+				snap.y = stepify(get_collision_point().y, 4)
+				snap.z = stepify(get_collision_point().z, 4)
+				
 				add_child(object_instance) # The instance is added as a child of the shoot node
 				object_instance.set_as_toplevel(true)
-				object_instance.translation = get_collision_point()
+				object_instance.translation = snap
--- Templates/3D/Abilities/AddObject/Object1.gd
+++ Templates/3D/Abilities/AddObject/Object1.gd
@@ -4,7 +4,3 @@ func _process(delta):
 	rotation_degrees.y = stepify(rotation_degrees.y, 90)
 	rotation.x = 0
 	rotation.z = 0
-	
-	translation.x = stepify(translation.x, 4)
-	translation.y = stepify(translation.y, 4)
-	translation.z = stepify(translation.z, 4)
