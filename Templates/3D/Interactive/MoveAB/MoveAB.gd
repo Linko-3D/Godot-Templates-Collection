3657bd8c431791aca45b5f7e93ffba490ff622af
Linko-3D <dbekhouc@gmail.com>
2019-10-07 16:49:47 +0200

    tweak

--- Templates/3D/Interactive/MoveAB/MoveAB.gd
+++ Templates/3D/Interactive/MoveAB/MoveAB.gd
@@ -13,15 +13,12 @@ var forward = true
 
 func _ready():
 	starting_position = translation
-	moveTo.x = starting_position.x + offsetX
-	moveTo.y = starting_position.y + offsetY
-	moveTo.z = starting_position.z + offsetZ
 
 func _process(delta):
 	if $Tween.is_active() == false: # Each time the animation stops check the direction set
 		if forward:
 			$Tween.start()
-			$Tween.interpolate_property(self, "translation", translation, moveTo, travel_seconds, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, start_pause)
+			$Tween.interpolate_property(self, "translation", translation, $Destination.global_transform.origin, travel_seconds, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, start_pause)
 			forward = false
 		else:
 			$Tween.start()
