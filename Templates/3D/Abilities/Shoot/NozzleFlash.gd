132fb58faaab0c9ac5a1fc6b305f0d36200907ba
Linko-3D <dbekhouc@gmail.com>
2019-10-08 22:53:46 +0200

    tweak

--- Templates/3D/Abilities/Shoot/NozzleFlash.gd
+++ Templates/3D/Abilities/Shoot/NozzleFlash.gd
@@ -1,14 +1,17 @@
 extends Position3D
 
 var hold_shoot = false
+var turned_On = false
+var fire_rate = 0.1
 
 func _process(delta):
+	print(turned_On)
 	if hold_shoot:
-		$OmniLight.light_energy = 1
-		$OmniLight.light_indirect_energy = 1
+		flash()
 	else:
 		$OmniLight.light_energy = 0
 		$OmniLight.light_indirect_energy = 0
+		turned_On = false
 
 func _input(event): 
 	if event is InputEventMouseButton and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
@@ -16,3 +19,15 @@ func _input(event):
 			hold_shoot = true
 		if event.button_index == 1 and event.pressed == false:
 			hold_shoot = false
+
+func flash():
+	if turned_On == false:
+		$OmniLight.light_energy = 1
+		$OmniLight.light_indirect_energy = 1
+		yield(get_tree().create_timer(fire_rate), "timeout")
+		turned_On = true
+	else:
+		$OmniLight.light_energy = 0
+		$OmniLight.light_indirect_energy = 0
+		yield(get_tree().create_timer(fire_rate), "timeout")
+		turned_On = false
