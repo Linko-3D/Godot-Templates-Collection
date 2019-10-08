b9f7a19ca2e967077565fa30ff501ebf4ebee5ea
Linko-3D <dbekhouc@gmail.com>
2019-10-08 20:51:00 +0200

    tweak

--- Templates/3D/Player/FirstPersonCharacter/FirstPersonCharacter.gd
+++ Templates/3D/Player/FirstPersonCharacter/FirstPersonCharacter.gd
@@ -71,7 +71,7 @@ func _input(event):
 
 func extra_abilities():
 	# Sprint:
-	if Input.is_action_pressed("sprint") and is_on_floor():
+	if Input.is_action_pressed("sprint"):
 		if vector.z < 0:						# If moving forward
 			vector.z *= sprint_speed_multiplier	# Run
 	
