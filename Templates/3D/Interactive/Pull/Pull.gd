29089c4988b94dece91fd61d910fbec3596f1b1e
Linko-3D <dbekhouc@gmail.com>
2019-10-13 00:22:15 +0200

    tweak

--- Templates/3D/Interactive/Pull/Pull.gd
+++ Templates/3D/Interactive/Pull/Pull.gd
@@ -3,8 +3,8 @@ extends RayCast
 var vector
 
 func _process(delta):
+	print(global_transform.origin)
 	if is_colliding():
 		if get_collider().get_class() == "RigidBody":
-			vector = (get_collider().translation - translation).normalized()
-			get_collider().translation -= vector
-			#print(translation)
+			vector = (get_collider().translation - global_transform.origin).normalized()
+			get_collider().translation -= vector * 0.1
