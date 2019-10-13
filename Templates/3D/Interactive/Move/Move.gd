eb9b7204291e35ff507bdf85eb5a44b002816bc3
Linko-3D <dbekhouc@gmail.com>
2019-10-13 23:07:04 +0200

    tweak

--- Templates/3D/Interactive/Move/Move.gd
+++ Templates/3D/Interactive/Move/Move.gd
@@ -5,7 +5,8 @@ export var end_pause = 0.0
 
 var vector
 
-func _process(delta):
+func _ready():
 	vector = global_transform.origin - $Destination.global_transform.origin
-	print(vector)
-	
+
+func _process(delta):
+	global_transform.origin -= vector * 0.003
