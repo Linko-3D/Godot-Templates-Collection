ec8da6bef70045cf428f1099ca98913db9aab898
Linko-3D <dbekhouc@gmail.com>
2019-10-14 18:03:04 +0200

    tweak

--- Templates/3D/AI/MoveTo/MoveTo.gd
+++ Templates/3D/AI/MoveTo/MoveTo.gd
@@ -11,17 +11,17 @@ var keepHeight = Vector3()
 
 func _ready():
 	target = get_tree().get_nodes_in_group("Player")[0] # Get the target with the first node in the Player group
-	keepHeight = translation.y
-	
+	keepHeight = global_transform.origin.y
+
 func _process(delta):
-	vector = (target.translation - translation).normalized() # Get the direction by subtracting the current position by the target position
-	
+	vector = (target.global_transform.origin - global_transform.origin).normalized() # Get the direction by subtracting the current position by the target position
+
 	if lock_linear_y:
-		translation.y = keepHeight # Keep the same height
-	
+		global_transform.origin.y = keepHeight # Keep the same height
+
 	move_and_slide(vector * speed)
-	
-	look_at(target.translation, Vector3.UP) # Look at the target
+
+	look_at(target.global_transform.origin, Vector3.UP) # Look at the target
 
 	if lock_angular_x:
 		rotation.x = 0
