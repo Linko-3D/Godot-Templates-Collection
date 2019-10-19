b04e369b04e75c5ae424ae9f7c90fc2422deb82b
Linko-3D <dbekhouc@gmail.com>
2019-10-19 22:44:06 +0200

    tweak

---
+++ Templates/Player/Projectile.gd
@@ -0,0 +1,12 @@
+extends RigidBody
+
+export var lifetime = 3.0
+var player
+
+func _ready():
+	player = get_tree().get_nodes_in_group("Player")[0]
+	add_collision_exception_with(player)
+	
+	if lifetime > 0:
+		yield(get_tree().create_timer(lifetime), "timeout")
+		queue_free()
