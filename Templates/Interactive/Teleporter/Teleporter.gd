57abda693438667d1bc8ddd774cd7f3ce799ccb7
Linko-3D <dbekhouc@gmail.com>
2019-10-20 17:34:34 +0200

    tweak

--- Templates/Interactive/Teleporter/Teleporter.gd
+++ Templates/Interactive/Teleporter/Teleporter.gd
@@ -2,4 +2,4 @@ extends Area
 
 func _on_Teleporter_body_entered(body):
 	if body.is_in_group("Player"):
-		body.translation = $Destination.global_transform.origin
+		body.global_transform.origin = $Destination.global_transform.origin
