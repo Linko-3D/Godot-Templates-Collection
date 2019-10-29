38212b2f3ee44ae0223384dfaf90ba8f26b7aade
Linko-3D <dbekhouc@gmail.com>
2019-10-29 22:26:14 +0100

    tweak

--- Templates/Player/Abilities/Shoot/SingleShot/SingleShot.gd
+++ Templates/Player/Abilities/Shoot/SingleShot/SingleShot.gd
@@ -1,7 +1,6 @@
 extends RayCast
 
 export var bullet_speed = 500.0
-export var bullet_lifetime = 0.1
 export (PackedScene) var bullet
 export (PackedScene) var nozzle_flash
 export (PackedScene) var shell
@@ -35,7 +34,7 @@ func _input(event):
 
 func show_hit():
 	$CrosshairHit.visible = true
-	yield(get_tree().create_timer(bullet_lifetime), "timeout")
+	yield(get_tree().create_timer(0.1), "timeout")
 	$CrosshairHit.visible = false
 
 func impact():
