39cdc57ee26af53848982bc9e461525821e09bbc
Linko-3D <dbekhouc@gmail.com>
2019-11-07 21:18:42 +0100

    tweak

--- Templates/Others/load_json.gd
+++ Templates/Others/load_json.gd
@@ -1,4 +1,17 @@
 extends Node
 
-var path = "user://saved_json.json"
+var path = "user://data.json"
 var file = File.new()
+
+var default_data = {"Value 1": 5}
+
+var data = { }
+
+func _ready():
+	save_game()
+
+func save_game():
+	file.open(path, File.WRITE)
+	
+	file.store_line(to_json(data))
+	file.close()
