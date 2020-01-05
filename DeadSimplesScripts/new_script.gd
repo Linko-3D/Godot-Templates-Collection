1bc9b1ac9c172ff59b17c21ab9bda485e3b4e239
Linko-3D <dbekhouc@gmail.com>
2020-01-05 11:39:39 +0100

    tweak

--- DeadSimplesScripts/new_script.gd
+++ DeadSimplesScripts/new_script.gd
@@ -8,18 +8,28 @@ var vector = Vector3()
 var gravity = 9.8
 
 func _physics_process(delta):
+	print(Input.get_action_strength("ui_up"))
+	
 	var direction = Vector2()
 	
-	direction.y += -Input.get_action_strength("ui_up") + Input.get_action_strength("ui_down")
-	direction.x += -Input.get_action_strength("ui_left") + Input.get_action_strength("ui_right")
+	direction.y -= Input.get_action_strength("ui_up")
 	
-	direction = direction.normalized()
+	#if Input.get_action_strength("ui_up"):
+	#	direction.y -= 1
+	#if Input.get_action_strength("ui_down"):
+	#	direction.y += 1
+	#if Input.get_action_strength("ui_left"):
+	#	direction.x -= 1
+	#if Input.get_action_strength("ui_right"):
+	#	direction.x += 1
+	
+	direction = direction.normalized().rotated(-rotation.y)
 	
 	vector.z = direction.y * speed
 	vector.x = direction.x * speed
 	vector.y -= gravity * delta
 	
-	vector = vector.rotated(Vector3.UP, rotation.y)
+	#vector = vector.rotated(Vector3.UP, rotation.y)
 	
 	if is_on_floor() and Input.is_action_just_pressed("ui_accept"):
 		vector.y = jump_force
