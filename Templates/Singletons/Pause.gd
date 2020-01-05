1616d0aaf68a6a98d6e07416cd81120c73140014
Linko-3D <dbekhouc@gmail.com>
2020-01-05 16:40:31 +0100

    tweak

--- Templates/Singletons/Pause.gd
+++ Templates/Singletons/Pause.gd
@@ -3,20 +3,14 @@
 extends Node
 
 func _ready():
-	pause_mode = PAUSE_MODE_PROCESS # This script can't get paused in order to resume or quit the game
+	pause_mode = PAUSE_MODE_PROCESS # This script can't get paused in order to resume the game
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _process(delta):
-	# Pause the game and shows the mouse, if the game was already paused it quits the game
 	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().paused = !get_tree().paused
+		
 		if get_tree().paused:
-			get_tree().quit()
-		get_tree().paused = true
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
-func _input(event):
-	# If the left mouse button is pressed the game is resumed
-	if event is InputEventMouseButton:
-		if event.button_index == 1 and event.pressed == true:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-			get_tree().paused = false
